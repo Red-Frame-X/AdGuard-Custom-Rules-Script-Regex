2225570c64a271debac6c931389d7c5e3ecaa934
Red Frame X <astray.redframe.mbf.p02@gmail.com>
2026-08-04 17:29:40 +0900

    Optimize youtube-shelf-force-expand.user.js

--- UserScript/youtube-shelf-force-expand.user.js
+++ UserScript/youtube-shelf-force-expand.user.js
@@ -2,7 +2,7 @@
 // @name         YouTube Shelf Force Expand
 // @namespace    http://tampermonkey.net/
 // @license      CC0-1.0
-// @version      1.0
+// @version      1.1.0
 // @description  YouTubeのシェルフの「もっと見る」を強制的に展開しボタンを隠す
 // @author       Red Frame X
 // @match        https://www.youtube.com/*
@@ -13,58 +13,46 @@
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    // 1. CSSルールの適用
-    // AdGuard: www.youtube.com##ytd-rich-shelf-renderer .button-container
     const css = `
         ytd-rich-shelf-renderer .button-container {
             display: none !important;
         }
     `;
 
-    if (typeof GM_addStyle !== 'undefined') {
+    if (typeof GM_addStyle === 'function') {
         GM_addStyle(css);
     } else {
         const style = document.createElement('style');
         style.textContent = css;
-        document.head.appendChild(style);
+        document.documentElement.appendChild(style);
     }
 
-    // 2. スクリプトレットのロジック（属性操作）
-    function applyAttributes() {
-        const hiddenItems = document.querySelectorAll('ytd-rich-item-renderer[hidden]');
-        if (hiddenItems.length > 0) {
-            for (const item of hiddenItems) {
-                item.removeAttribute('hidden');
-            }
+    const applyAttributes = () => {
+        for (const item of document.querySelectorAll('ytd-rich-item-renderer[hidden]')) {
+            item.removeAttribute('hidden');
         }
 
-        const shelves = document.querySelectorAll('ytd-rich-shelf-renderer:not([is-show-more-hidden])');
-        if (shelves.length > 0) {
-            for (const shelf of shelves) {
-                shelf.setAttribute('is-show-more-hidden', '');
-            }
+        for (const shelf of document.querySelectorAll('ytd-rich-shelf-renderer:not([is-show-more-hidden])')) {
+            shelf.setAttribute('is-show-more-hidden', '');
         }
-    }
+    };
 
-    // 3. 監視設定 (YouTubeはSPAで動的に要素が増えるため)
-    const observer = new MutationObserver((mutations) => {
-        let shouldProcess = false;
-        for (const mutation of mutations) {
-            if (mutation.addedNodes.length > 0 || mutation.type === 'attributes') {
-                shouldProcess = true;
-                break;
-            }
-        }
+    let frameId = 0;
+    const scheduleApply = () => {
+        if (frameId) return;
 
-        if (shouldProcess) {
+        frameId = requestAnimationFrame(() => {
+            frameId = 0;
             applyAttributes();
-        }
-    });
+        });
+    };
 
-    window.addEventListener('load', () => {
+    const observer = new MutationObserver(scheduleApply);
+
+    const start = () => {
         applyAttributes();
         observer.observe(document.body, {
             childList: true,
@@ -72,8 +60,11 @@
             attributes: true,
             attributeFilter: ['hidden']
         });
-    });
+    };
 
-    applyAttributes();
-
+    if (document.body) {
+        start();
+    } else {
+        document.addEventListener('DOMContentLoaded', start, { once: true });
+    }
 })();
